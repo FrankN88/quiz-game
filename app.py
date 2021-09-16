0284e53538da1f2f3e3a63ca06d72983591b9458
FrankN88 <nardecchia.f@gmail.com>
2021-09-16 17:25:31 +0200

    Include env file and add basic templates

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 from flask_pymongo import PyMongo
+import os
+if os.path.exists("env.py"):
+    import env
 
 # ==============================================================
-# Config access to MongoDB
+# Config access to MongoDB with .env file
 # ==============================================================
 app = Flask(__name__)
-app.config["MONGO_URI"] = os.environ['MONGO_URI']
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
 mongo = PyMongo(app)
 
 # ==============================================================
