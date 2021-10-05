1e91479929a941e56fbafe939488a9117a58c2bd
FrankN88 <nardecchia.f@gmail.com>
2021-10-05 16:20:12 +0200

    footer.html - Adding js warning for cancellation

---
+++ static/js/admin.js
@@ -0,0 +1,9 @@
+function delete_question(id){
+      if(!confirm("Delete this element?")) return;
+      window.open("/delete_question?id_question="+id,"_self");
+}
+
+function delete_user(id){
+      if(!confirm("Delete this element?")) return;
+      window.open("/delete_user?id_user="+id,"_self");
+}
