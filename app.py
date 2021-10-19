d524fc1f0defcf4f85ceac110545d123d726056d
FrankN88 <nardecchia.f@gmail.com>
2021-10-19 10:11:24 +0200

    app.py - Create function to show image from AWS

--- app.py
+++ app.py
@@ -425,6 +425,25 @@ def upload_file(file_name, bucket):
     return response
 
 
+# Show image in bucket on AWS
+def show_image(bucket,id_user):
+    s3_client = boto3.client('s3')
+    url = ""
+    
+    try:
+        for item in s3_client.list_objects(Bucket=bucket)['Contents']:
+            presigned_url = s3_client.generate_presigned_url('get_object', Params = {'Bucket': bucket, 'Key': item['Key']}, ExpiresIn = 100)
+
+            if presigned_url.find(id_user) != -1:
+                url = presigned_url
+
+    except Exception as e:
+        pass
+
+    # Return URL of the image
+    return url
+    
+
 # Questions page (only for admin)
 @app.route("/questions", methods=["POST", "GET"])
 def questions():
